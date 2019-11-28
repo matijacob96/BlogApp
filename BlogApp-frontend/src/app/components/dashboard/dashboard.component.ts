994beacda79743448f3f6b8c29ed4c927da54400
Matías Jacob <matijacob@live.com>
2019-11-28 14:02:56 -0300

    Finishing Part IV

--- BlogApp-frontend/src/app/components/dashboard/dashboard.component.ts
+++ BlogApp-frontend/src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { PostService } from '../../services/post.service';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,10 +11,18 @@ import { PostService } from '../../services/post.service';
 export class DashboardComponent implements OnInit {
   posts: any[];
 
-  constructor( private postService: PostService) { }
+  constructor( private postService: PostService,
+    private auth: AuthService,
+    private router: Router) { }
 
   ngOnInit() {
+    
     this.getPosts();
+  }
+
+  logout(){
+    this.auth.logout();
+    this.router.navigate(['']);
   }
 
   getPosts() {
